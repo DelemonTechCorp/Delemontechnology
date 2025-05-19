05a576867d91d3eda890ae809d1b2ce55d177455
cthridya <cthridya@gmail.com>
2025-05-19 12:05:53 +0530

    done

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,6 +140,6 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='delemonjobs@gmail.com'
-EMAIL_HOST_PASSWORD='ncog dcjr ksry chgu'
+EMAIL_HOST_PASSWORD='ixbs wabm hgyc erie'
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
