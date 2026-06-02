f9a074e6fdad1cfc2b2ab195d0f97e19f366a87b
aparna <ekaparna3@gmail.com>
2026-06-02 10:56:15 +0530

    updating

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-45&z!*+v@&l%d&9l8kp@tf0(0q^wg4tmj!2(4nb^t^c6@7f$$7'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
 # ALLOWED_HOSTS = ['www.delemontechnology.com','*']
